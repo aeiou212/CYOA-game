4abe4aaea940b5bc8ed5ff3cdd8b96e5d13f4e4f
CarlosCodeIT <carlosav3698@gmail.com>
2026-04-28 19:57:45 +0000

    changed room.cpp

--- Room.cpp
+++ Room.cpp
@@ -1,5 +1,5 @@
 #include "Room.h"
-
+//room number
 Room::Room(std::string n, std::string d, std::vector<std::string> a, Item i) 
     : name(n), description(d), actions(a), item(i), searched(false) {}
 
